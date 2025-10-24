19714487c8a5cf0fbdfa6427a5be326c0d54f105
Manuel Enrique Garcia Neve <77370606+many903@users.noreply.github.com>
2025-10-24 16:34:42 -0600

    objetos
    faltan códigos de ejecución

---
+++ 2193077979/objetos_viernes/persona/Persona.java
@@ -0,0 +1,40 @@
+package objetos_viernes.persona;
+
+public class Persona {
+    //atributos 
+    string Nombre;
+    int Edad;
+    string Genero;
+    
+    //metodo constructor
+    public Persona(srting nombre, int edad, string genero){
+        this.Nombre = nombre;
+        this.Edad = edad;
+        this.Genero = genero;
+    }
+    
+    //metodos
+    public void set edad (){
+        this.Edad = edad 
+    
+    }
+    public void get Edad(){
+        return Edad;
+    }
+    public void set Nombre (){
+        this.Nombre = nombre; 
+    
+    }
+    public void get Nombre(){
+        return Nombre;
+    } 
+        public void set Genero (){
+        this.Genero = genero;
+    
+    }
+    public void get Genero(){
+        return Edad;
+    }
+    
+}
+

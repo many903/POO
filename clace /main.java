c2e558eaa649fc26f86ce797f0b5f319b126b2bb
Manuel Enrique Garcia Neve <77370606+many903@users.noreply.github.com>
2025-10-27 22:10:36 +0000

    maon clase cubo carpeta objetos/modificacion

---
+++ clace /main.java
@@ -0,0 +1,12 @@
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("objeto vacio");
+        Cubo cubo1 = new Cubo();
+        cubo1.mostrar();
+        System.out.println("--------------------");
+        System.out.println("objeto con dos atributos");
+        Cubo cubo2 = new Cubo("Grande", "Rojo");
+        cubo2
+    }
+}

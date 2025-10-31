55ae8e964513d9b84e8c6b5c766bfaddb42dec9d
Manuel Enrique Garcia Neve <77370606+many903@users.noreply.github.com>
2025-10-31 22:07:59 +0000

    main heredado completo

--- herencia/main.java
+++ herencia/main.java
@@ -1,1 +1,8 @@
-public class 
+public class main {
+    public static void main(String[] args) {
+        Hijo Heredado = new Heredado ("uan papa", "pedro Hijo");
+        System.out.println("---------");
+        System.out.println("clase heredado"+ heredado.getAtributo);
+        System.out.println("clase heredado"+ heredado.getAtributos2);
+    }
+}
